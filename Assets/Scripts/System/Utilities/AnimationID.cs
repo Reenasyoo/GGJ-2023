dc9148bf44de1b932aabc122b8271eb72f8f72b6
Eric <lil.reenas@gmail.com>
2023-02-04 20:13:03 +0200

    player animations

--- Assets/Scripts/System/Utilities/AnimationID.cs
+++ Assets/Scripts/System/Utilities/AnimationID.cs
@@ -6,5 +6,6 @@ namespace Systems.Utilities
         public static readonly int FORWARD_VELOCITY = Animator.StringToHash("ForwardVelocity");
         public static readonly int STRAFE_VELOCITY = Animator.StringToHash("StrafeVelocity");
         public static readonly int CHARACTER_ROTATION_Y = Animator.StringToHash("CharacterRotation_Y");
+        public static readonly int PICKUP_TRIGGER = Animator.StringToHash("Pickup");
     }
 }
